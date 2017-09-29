a45d36b83daa083704186c52b501728babf54f69
Efim Gotsko <gotsco-en@yandex.ru>
2017-09-29 10:45:38 +0400

    Progress on stylelinting. Tried ExtractTextPlugin, styleling-loader for linting but did not succeed, went with postcss-loader

--- config/paths.js
+++ config/paths.js
@@ -2,5 +2,6 @@ const { join, resolve } = require('path');
 
 module.exports = {
   html: resolve(process.cwd(), 'src/index.html'),
+  styles: resolve(process.cwd(), 'src/styles.css'),
   build: resolve(process.cwd(), '.dist')
 };
--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,12 +1,16 @@
 const { LoaderOptionsPlugin } = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const StyleLintWebpackPlugin = require('stylelint-webpack-plugin');
 const {
   build,
   html,
+  styles
 } = require('./paths');
 
 const config = {
   entry: {
-    'index.html': html
+    'index.html': html,
+    styles: styles
   },
   devtool: 'source-map',
   output: {
@@ -31,11 +35,45 @@ const config = {
         use: {
           loader: 'html-loader'
         }
-      }
+      },
+      // {
+      //   test: /\.css/,
+      //   use: [
+      //     'css-loader'
+      //   ]
+      // }
+      {
+        test: /\.css/,
+        use: [
+          // ExtractTextPlugin.extract({
+          //   fallback: 'style-loader',
+          //   use: 'css-loader'
+          // }),
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                require('stylelint')()
+              ]
+            }
+          }
+        ]
+      },
+      // {
+      //   test: /\.(png|jpg|jpeg|gif|svg|eot|ttf|woff|woff2)$/,
+      //   loader: 'url-loader',
+      //   options: {
+      //     limit: 10000
+      //   }
+      // }
     ]
   },
   plugins: [
-    new LoaderOptionsPlugin({ debug: true })
+    new LoaderOptionsPlugin({ debug: true }),
+    new ExtractTextPlugin({
+      filename: '[name].css'
+    }),
+    new StyleLintWebpackPlugin({ ignoreDisables: true })
   ]
 };
 
