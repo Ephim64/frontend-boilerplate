bf3db5d2c6fbb8f1c0519dea7545cbf8cbe9f76b
Ephim <gotsco-en@yandex.ru>
2017-10-21 18:48:32 +0400

    small tweakings

--- config/paths.js
+++ config/paths.js
@@ -1,18 +1,18 @@
-const { join, resolve } = require('path');
-const { realpathSync } = require('fs');
+const { join, resolve } = require("path");
+const { realpathSync } = require("fs");
 
 const root = realpathSync(process.cwd());
 const resolveRoot = relativePath => resolve(root, relativePath);
 
 module.exports = {
-  html: resolveRoot('src/index.html'),
-  stylesScss: resolveRoot('src/sass/index.scss'),
-  stylesCss: resolveRoot('src/css/styles.css'),
-  js: resolveRoot('src/js/scripts.js'),
-  build: resolveRoot('.dist'),
-  spriteImages: resolveRoot('src/content/icons'),
-  spritesmithGenerated: resolveRoot('src/spritesmith-generated'),
-  smTarget: resolveRoot('src/spritesmith-generated/sprite.png'),
-  smCss: resolveRoot('src/spritesmith-generated/sprite.scss'),
-  loaders: resolveRoot('config/loaders')
+  html: resolveRoot("src/index.html"),
+  stylesScss: resolveRoot("src/sass/index.scss"),
+  stylesCss: resolveRoot("src/css/styles.css"),
+  js: resolveRoot("src/js/scripts.js"),
+  build: resolveRoot(".dist"),
+  spriteImages: resolveRoot("src/content/icons"),
+  spritesmithGenerated: resolveRoot("src/spritesmith-generated"),
+  smTarget: resolveRoot("src/spritesmith-generated/sprite.png"),
+  smCss: resolveRoot("src/spritesmith-generated/sprite.css"),
+  loaders: resolveRoot("config/loaders")
 };
