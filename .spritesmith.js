86f3aaf8391b866b90919a25d3a5345b111d4762
Ephim <gotsco-en@yandex.ru>
2018-01-12 02:09:53 +0400

    fix file extension

--- .spritesmith.js
+++ .spritesmith.js
@@ -1,5 +1,5 @@
 module.exports = {
-  src: './src/content/icons/**/!(sprite).{png,git,jpg}',
+  src: './src/content/icons/**/!(sprite).{png,gif,jpg}',
   destImage: './src/content/icons/sprite.png',
   destCSS: './src/sass/base/sprite.scss'
 };
