f99b6cd70544ed13d936829891e005362dfff258
Efim Gotsko <gotsco-en@yandex.ru>
2018-01-15 10:27:58 +0400

    Fix script

--- scripts/htmllint.js
+++ scripts/htmllint.js
@@ -4,22 +4,21 @@ const chalk = require('chalk');
 const htmllint = require('htmllint');
 
 const htmlPath = resolve(process.cwd(), process.argv[2]);
-const parsed = parse(process.argv[2]);
-const filename = parsed.name + parsed.ext;
 
 const html = readFileSync(htmlPath, { encoding: 'utf-8' });
-
 const result = htmllint(html);
 
 result.then(issues => {
-  const header = chalk.underline.yellow(`${filename}:`);
-  console.log(header);
-  issues.forEach(issue => {
-    const msg = [
-      chalk.white(`\t${issue.line}:${issue.column}`),
-      chalk.red(`\t${issue.rule}`),
-      chalk.white(`\t${htmllint.messages.renderIssue(issue)}`)
-    ].join('');
-    console.log(chalk.red(msg));
-  });
+    const header = chalk.underline.yellow(`${parse(htmlPath).base}:`);
+
+    console.log(header);
+    issues.forEach(issue => {
+        const msg = [
+            chalk.white(`\t${issue.line}:${issue.column}`),
+            chalk.red(`\t${issue.rule}`),
+            chalk.white(`\t${htmllint.messages.renderIssue(issue)}`)
+        ].join('');
+
+        console.log(chalk.red(msg));
+    });
 }, console.log);
